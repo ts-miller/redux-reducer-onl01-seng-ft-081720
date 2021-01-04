350b80584968278dd1c6a4b3c43818d2f586231f
ts-miller <tylerstephen11@gmail.com>
2021-01-04 14:53:02 -0500

    commit tree 41434aed6b53c4c8591c819b020eb589e861ffda

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {friends: [...state.friends, action.friend]}
+            break
+        case "REMOVE_FRIEND":
+            return {friends: state.friends.filter(x => x.id !== action.id)}
+            break
+        default:
+            return state
+    }
 }
